5b87849287dbb5e8b24ce1716e1ac8b3860b8ad7
xiaohe27 <xiaoguoyi27@gmail.com>
2015-10-07 21:10:49 -0500

    polish the regex

--- src/main/java/parser/annotation/Patterns.java
+++ src/main/java/parser/annotation/Patterns.java
@@ -22,8 +22,11 @@ public class Patterns {
 
     public static final Pattern POST_COND_PATTERN = Pattern.compile(postCondPattern);
 
+    public static final String CLAUSE =
+            "(requires|ensures)\\p{Blank}+([\\p{Print}\\p{Blank}&&[^;]]*);";
+
     public static final Pattern METHOD_CONTRACT =
-            Pattern.compile("/\\*\\*@\\p{Space}+(((requires)|(ensures))\\p{Blank}+([\\p{Print}\\p{Blank}&&[^;]]*);\\p{Space}*)*@\\*/");
+            Pattern.compile("/\\*\\*@\\p{Space}+((" + CLAUSE + "\\p{Space}*)*)@\\*/");
 
     public static void main(String[] args) {
         String input = "/**@" +
@@ -32,21 +35,30 @@ public class Patterns {
                 "\nensures d == a + b + c;\n@*/";
 
         Matcher matcher = METHOD_CONTRACT.matcher(input);
-        int count = 1;
+        int count = 0;
         int groupSize = matcher.groupCount();
         System.out.println("Group size is " + groupSize);
 
-        matcher.find();
-        while(count <= groupSize) {
-            String groupI = matcher.group(count++);
-            System.out.println("group " + count + ": " + groupI);
+        String contractStr = null;
+        if(matcher.find()) {
+            contractStr = matcher.group(1);
+        }
+
+        System.out.println("Contract is:\n" + contractStr);
+
+        matcher = Pattern.compile(CLAUSE).matcher(contractStr);
+
+        while (matcher.find()) {
+            System.out.println("No. " + (count++) + " clause: " + matcher.group(0));
+            String groupI = matcher.group(1);
+
             switch (groupI) {
-                case REQUIRE :
-                    System.out.println("@pre : " + matcher.group(count++));
+                case REQUIRE:
+                    System.out.println("@pre : " + matcher.group(2));
                     break;
 
-                case ENSURE :
-                    System.out.println("@post : " + matcher.group(count++));
+                case ENSURE:
+                    System.out.println("@post : " + matcher.group(2));
                     break;
 
                 default:
