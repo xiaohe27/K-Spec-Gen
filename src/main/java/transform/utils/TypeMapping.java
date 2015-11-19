ad62b812cf05ee7e832e43e6bb7613eaaa4171c7
xiaohe27 <xiaoguoyi27@gmail.com>
2015-11-19 10:12:01 -0600

    add type mapping class

--- src/main/java/transform/utils/TypeMapping.java
+++ src/main/java/transform/utils/TypeMapping.java
@@ -1,7 +1,13 @@
 package transform.utils;
 
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  * Created by hx312 on 19/11/2015.
  */
 public class TypeMapping {
+    public static String convert2KType(SingleVariableDeclaration v) {
+        throw new NotImplementedException();
+    }
 }
