6a618c94ff9f0cbfad25a121f599bba65ad9d24f
xiaohe27 <xiaoguoyi27@gmail.com>
2015-11-21 11:04:44 -0600

    gen range constraint

---
+++ src/main/java/transform/utils/ConstraintGen.java
@@ -0,0 +1,10 @@
+package transform.utils;
+
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
+
+/**
+ * Created by hx312 on 21/11/2015.
+ */
+public class ConstraintGen {
+    public static String genRangeConstraint4Type(SingleVariableDeclaration)
+}
