0799f683f89d2570d430890b6b5ed537686b9edf
xiaohe27 <xiaoguoyi27@gmail.com>
2015-11-24 23:17:15 -0600

    modify bst.java

--- src/main/resources/debugPurpose/simple/bst.java
+++ src/main/resources/debugPurpose/simple/bst.java
@@ -3,7 +3,7 @@ public class bst {
     bst left;
     bst right;
 
-/*
+
   public static void main(String[] args) {
   bst node1 = new bst (0);
 bst node2 = new bst (0);
@@ -11,8 +11,12 @@ bst node3 = new bst (0);
 node1.left = node2;
 node1.right = node3;
 }
-*/
 
+
+    /**
+     * @requires bst(t);
+     *
+     */
     static boolean find(int v, bst t) {
         if (t == null)
             return false;
