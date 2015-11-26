68ab74b7ecfe58eeeb9016389855606a2eee8558
xiaohe27 <xiaoguoyi27@gmail.com>
2015-11-26 14:36:09 -0600

    add testcase stub for expr transformer

--- src/test/java/transform/utils/TypeMappingTest.java
+++ src/test/java/transform/utils/TypeMappingTest.java
@@ -2,18 +2,40 @@ package transform.utils;
 
 import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.ArrayList;
-import mockit.*;
+import java.util.Arrays;
+import java.util.Collection;
+import static org.junit.Assert.*;
 
 /**
  * Created by hx312 on 23/11/2015.
  */
+@RunWith(Parameterized.class)
 public class TypeMappingTest {
     ArrayList<SingleVariableDeclaration> formalParams;
 
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {"int a, int b", "a < b", "A <Int B"}
+        });
+    }
+
+    private String formalParamsStr;
+    private String javaExpStr;
+    private String kExpStr;
+
+    public TypeMappingTest(String formalParamsStr, String javaExpStr, String kExpStr) {
+        this.formalParamsStr = formalParamsStr;
+        this.javaExpStr = javaExpStr;
+        this.kExpStr = kExpStr;
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -23,14 +45,10 @@ public class TypeMappingTest {
 
     @After
     public void tearDown() throws Exception {
-
+        this.formalParams.clear();
     }
 
     @Test
-    public void testFromJExpr2KExpr(@Mocked final SingleVariableDeclaration varA) throws Exception {
-
-        new Expectations(){{
-            varA.toString(); result = "int";
-        }};
+    public void testFromJExpr2KExpr() throws Exception {
     }
 }
