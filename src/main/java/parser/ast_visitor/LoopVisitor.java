4c934d6859192d5cb72afc7f33f8ffefe1da6f46
xiaohe27 <xiaoguoyi27@gmail.com>
2015-11-29 11:34:22 -0600

    update

--- src/main/java/parser/ast_visitor/LoopVisitor.java
+++ src/main/java/parser/ast_visitor/LoopVisitor.java
@@ -1,13 +1,13 @@
 package parser.ast_visitor;
 
-import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.IBinding;
-import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.*;
 
 /**
  * Created by hx312 on 28/11/2015.
  */
 public class LoopVisitor extends ASTVisitor {
+    private StringBuilder kAST = new StringBuilder();
+
     public boolean visit(SimpleName name) {
         IBinding binding = name.resolveTypeBinding();
 
@@ -16,4 +16,17 @@ public class LoopVisitor extends ASTVisitor {
 
         return false;
     }
+
+    public boolean visit(WhileStatement whileNode) {
+        //TODO
+        return true;
+    }
+
+    public boolean visit(Assignment assignmentNode) {
+        //TODO
+        System.out.println("Visit assignment stmt: " + assignmentNode.toString());
+        return true;
+    }
+
+
 }
--- src/main/java/transform/utils/KAST_Transformer.java
+++ src/main/java/transform/utils/KAST_Transformer.java
@@ -6,7 +6,32 @@ import org.eclipse.jdt.core.dom.Expression;
  * Created by hx312 on 29/11/2015.
  */
 public class KAST_Transformer {
-    public static String convert2KAST(Expression expr) {
+    /**
+     * Convert a java expression ast to k expression ast.
+     *
+     * @param jexp
+     * @return
+     */
+    public static String convert2KAST(Expression jexp) {
+        switch (jexp.getNodeType()) {
+            case Expression.INFIX_EXPRESSION:
+                break;
+
+            case Expression.PREFIX_EXPRESSION:
+                break;
+
+            //The var name expr
+            case Expression.SIMPLE_NAME:
+                break;
+
+            //The constants
+            case Expression.NUMBER_LITERAL:
+
+            case Expression.STRING_LITERAL:
+
+            case Expression.BOOLEAN_LITERAL:
+                break;
+        }
 
         return "";
     }
