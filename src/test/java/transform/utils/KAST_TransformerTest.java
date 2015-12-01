d8b56176f77d2cb47d1e3c17218b071adbd90ebb
xiaohe27 <xiaoguoyi27@gmail.com>
2015-12-01 16:32:15 -0600

    add test stub

---
+++ src/test/java/transform/utils/KAST_TransformerTest.java
@@ -0,0 +1,28 @@
+package transform.utils;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hx312 on 12/1/2015.
+ */
+public class KAST_TransformerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testConvert2KAST() throws Exception {
+
+    }
+}
