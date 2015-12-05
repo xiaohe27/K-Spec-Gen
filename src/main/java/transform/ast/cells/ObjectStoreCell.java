515e7a321d89b0a9012f78906490bd8327ba8514
xiaohe27 <xiaoguoyi27@gmail.com>
2015-12-04 18:17:52 -0600

    toString() of ObjectStoreCell

--- src/main/java/transform/ast/cells/ObjectStoreCell.java
+++ src/main/java/transform/ast/cells/ObjectStoreCell.java
@@ -1,15 +1,34 @@
 package transform.ast.cells;
 
-import java.util.HashMap;
+import transform.ast.rewrite.KRewriteObj;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by xiaohe on 11/1/15.
  */
 public class ObjectStoreCell extends Cell {
-    private final HashMap<String, String> heap;
+    private final List<KRewriteObj> objStores = new ArrayList<>();
 
-    public ObjectStoreCell() {
+    public ObjectStoreCell(List<KRewriteObj> objStores0) {
         super(Cell.OBJ_STORE);
-        this.heap = new HashMap<>();
+        if (objStores0 != null)
+            this.objStores.addAll(objStores0);
+    }
+
+    @Override
+    public String toString() {
+        if (this.objStores.isEmpty())
+            return "";
+
+        StringBuilder sb = new StringBuilder();
+
+        this.objStores.forEach(kRewriteObj -> {
+            sb.append(kRewriteObj.toString() + "\n");
+        });
+
+        sb.append(" (.Bag => ?_:Bag) ");
+        return super.surroundWithTags(sb.toString());
     }
 }
