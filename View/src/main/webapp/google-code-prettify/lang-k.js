3260e1414a853048e479659d1a682878b39a18c5
xiaohe27 <xiaoguoyi27@gmail.com>
2015-12-15 17:57:03 -0600

    add color

--- View/src/main/webapp/google-code-prettify/lang-k.js
+++ View/src/main/webapp/google-code-prettify/lang-k.js
@@ -2,4 +2,5 @@ var a=null;
 PR.registerLangHandler(PR.createSimpleLexer([["opn",/^\(+/,a,"("],["clo",/^\)+/,a,")"],["com",/^;[^\n\r]*/,a,";"],["pln",/^[\t\n\r \xa0]+/,a,"\t\n\r \u00a0"],["str",/^"(?:[^"\\]|\\[\S\s])*(?:"|$)/,a,'"']],[["kwd",/^(?:require|throw|module|imports|rule)\b/,a],
     ["javak",/^(?:while|if|else|break|continue|class|return)\b/,a],
     ["javatype",/^(?:int|bool|float|string)\b/,a],
-["lit",/^[+-]?(?:[#0]x[\da-f]+|\d+\/\d+|(?:\.\d+|\d+(?:\.\d*)?)(?:[de][+-]?\d+)?)/i],["lit",/^'(?:-*(?:\w|\\[!-~])(?:[\w-]*|\\[!-~])[!=?]?)?/],["pln",/^-*(?:[_a-z]|\\[!-~])(?:[\w-]*|\\[!-~])[!=?]?/i],["pun",/^[^\w\t\n\r "'-);\\\xa0]+/]]),["k"]);
+    ["kcell",/^(?:k|env|threads|thread|classes|NumOfClassesToUnfold|program|globalPhase|store|busy|nextLoc|objectStore)\b/,a],
+    ["lit",/^[+-]?(?:[#0]x[\da-f]+|\d+\/\d+|(?:\.\d+|\d+(?:\.\d*)?)(?:[de][+-]?\d+)?)/i],["lit",/^'(?:-*(?:\w|\\[!-~])(?:[\w-]*|\\[!-~])[!=?]?)?/],["pln",/^-*(?:[_a-z]|\\[!-~])(?:[\w-]*|\\[!-~])[!=?]?/i],["pun",/^[^\w\t\n\r "'-);\\\xa0]+/]]),["k"]);
