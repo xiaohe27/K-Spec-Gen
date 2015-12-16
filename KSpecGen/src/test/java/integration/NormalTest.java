743acf39d803b64ef837287173ecd0cf6289194a
xiaohe27 <xiaoguoyi27@gmail.com>
2015-12-16 11:58:16 -0600

    mixed annotation test

--- KSpecGen/src/test/java/integration/NormalTest.java
+++ KSpecGen/src/test/java/integration/NormalTest.java
@@ -11,8 +11,6 @@ import transform.Main;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
 
