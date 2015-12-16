74a0935ff4d960c8b22373eeb94894409547d3f8
xiaohe27 <xiaoguoyi27@gmail.com>
2015-12-16 11:00:35 -0600

    add test cases

---
+++ KSpecGen/src/test/java/integration/NormalTest.java
@@ -0,0 +1,44 @@
+package integration;
+
+import common.BasicTest;
+import common.Const;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import transform.Main;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by xiaohe on 12/16/15.
+ */
+@RunWith(Parameterized.class)
+public class NormalTest extends BasicTest {
+    public NormalTest(int testId) {
+        super(testId, Const.inExt, Const.outExt);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {7}, {8}, {9}, {10}
+        });
+    }
+
+    @Test
+    public void testOutput() throws IOException {
+        String expectedStr = new String(Files.readAllBytes(super.expectedFile.toPath()))
+                .trim().replaceAll("\\p{Space}", "");
+        Main.setAllowCache();
+        Main.main(new String[]{super.inputFile.getAbsolutePath()});
+
+        assertEquals("The generated k-spec is not the same as expected.",
+                expectedStr,
+                Main.getCachedResult().trim().replaceAll("\\p{Space}", ""));
+    }
+}
