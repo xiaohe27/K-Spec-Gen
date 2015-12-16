4f914d7ea84588eb536f18fe5b5c698e12e93bdd
xiaohe27 <xiaoguoyi27@gmail.com>
2015-12-16 11:24:15 -0600

    add tearDown method

--- KSpecGen/src/test/java/integration/NormalTest.java
+++ KSpecGen/src/test/java/integration/NormalTest.java
@@ -2,6 +2,8 @@ package integration;
 
 import common.BasicTest;
 import common.Const;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -9,6 +11,8 @@ import transform.Main;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -30,11 +34,23 @@ public class NormalTest extends BasicTest {
         });
     }
 
+    @Before
+    public void setUp() {
+        Main.setAllowCache();
+    }
+
+    @After
+    public void tearDown() {
+        Path kspecPath = Paths.get(super.inputFile.getAbsolutePath() + ".k");
+        if (kspecPath != null && kspecPath.toFile().exists()) {
+            kspecPath.toFile().delete();
+        }
+    }
+
     @Test
     public void testOutput() throws IOException {
         String expectedStr = new String(Files.readAllBytes(super.expectedFile.toPath()))
                 .trim().replaceAll("\\p{Space}", "");
-        Main.setAllowCache();
         Main.main(new String[]{super.inputFile.getAbsolutePath()});
 
         assertEquals("The generated k-spec is not the same as expected.",
